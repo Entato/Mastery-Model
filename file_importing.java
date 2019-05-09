0c0fed766ef26338f2bbe3ec0cacc4eeb48ce646
Frank <50030060+Zaperex@users.noreply.github.com>
2019-05-09 14:51:17 -0400

    Update file_importing.java

--- file_importing.java
+++ file_importing.java
@@ -28,7 +28,7 @@ class file_importing{
         String[] temprowarray = line.split(","); //Stores the seperated values of each row into an array
         student_csv_Storer.add(temprowarray); //Adds array into an arraylist
       }
-      filereader.close();
+      filereader.close(); //Closes filereader
     }
     catch (IOException e){ //If a file wasn't found
       System.out.println("A file was not found.");
@@ -47,23 +47,59 @@ class file_importing{
       String line = " "; //Intializes line variable
       BufferedReader filereader = new BufferedReader(new FileReader(chart_Template)); //Bufferedreader to read the file
       while ((line = filereader.readLine())!= null){ //Keeps running until all lines are read
-        String[] temprowarray = line.split(","); //Stores the seperated values of each row into an array
+        String newline = line.replaceAll(", ", "#####"); //Replaces the commas that are not delimiters
+        String[] temprowarray = newline.split(","); //Stores the seperated values of each row into an array
         chart_template_Storer.add(temprowarray); //Adds array into an arraylist
       }
-      filereader.close();
+      filereader.close(); //Closes filereader
     }
     catch (IOException e){ //If a file wasn't found
       System.out.println("A file was not found.");
     }
     
+    //Prints out content of the csv for testing purposes
     for (int i = 0; i < chart_template_Storer.size(); i++){
       for (int j = 0; j < chart_template_Storer.get(i).length; j++){
+        //Replaces the temporary characters with it's old character
+        chart_template_Storer.get(i)[j] = chart_template_Storer.get(i)[j].replaceAll("#####", ", "); 
         System.out.println(chart_template_Storer.get(i)[j]);
       }
+      System.out.println("_______________________________________________________________________");
     }
     
-  
+    //Finds the index number of a student in the array
+    Scanner input = new Scanner(System.in);
+    String name_Input = input.nextLine();
+    int index_number = 0;
     
+    for (int i = 0; i < student_csv_Storer.size(); i++){
+      if (name_Input.equals(student_csv_Storer.get(i)[1] + ", " + student_csv_Storer.get(i)[2])){
+        index_number = i;
+      }
+    }
+    String student_Path = (MainFolderPath + "\\" + student_csv_Storer.get(index_number)[0]);
+    System.out.println(student_Path);
+    
+    //_____________TEST PORTION______________________________________________
+    File Test_File = new File(MainFolderPath + "\\Test_File.csv");
+    
+    if (Test_File.createNewFile()){
+      System.out.println("FILE CREATED!");
+    }
+    else{
+      System.out.println("No File Created");
+    }
+    
+    //FileWriter writer = new FileWriter(Test_File, true); //True states that this is appending existing file
+    FileWriter writer = new FileWriter(Test_File, false); //False states that this is overwriting existing file
+    for (int i = 0; i < 10; i++){
+      writer.append("Hello");
+      writer.append(","); //Seperates values in csv
+      writer.append("boo");
+      writer.append("\n"); //Moves to next row
+    }
+    writer.close();
+    //_________________________________________________________________________
     
     
     
